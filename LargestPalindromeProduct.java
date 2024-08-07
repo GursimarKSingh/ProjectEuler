267e2b512d3a7454d593068e31765ba7c6eaeeee
Gursimar Singh <139287880+GursimarKSingh@users.noreply.github.com>
2024-08-07 14:38:38 -0400

    Add files via upload

---
+++ LargestPalindromeProduct.java
@@ -0,0 +1,39 @@
+public class LargestPalindromeProduct {
+
+
+    public static void main (String[] args){
+
+        int largestnum = 0;
+
+
+        for (int i = 1; i < 1000; i++){
+            for(int j = 1; j < 1000; j++){
+
+                int product = i* j;
+                String str = Integer.toString(product);
+
+                if(isPalindrome(str)){
+                    largestnum = product;
+                }
+
+            }
+        }
+
+        StdOut.print(largestnum);
+    }
+
+    public static boolean isPalindrome(String str){
+        for(int i = 0; i < str.length()/2; i++){
+
+            if(str.charAt(i)== str.charAt(str.length()-1-i)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
